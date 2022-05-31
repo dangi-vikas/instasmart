899c487a341e21f48484d07a855cc61c0fb7da4e
dangi-vikas <mvikasdangi@gmail.com>
2022-05-31 13:36:40 +0530

    fix: login service functioning fixed

--- src/main/java/com/customer/service/LoginServiceImpl.java
+++ src/main/java/com/customer/service/LoginServiceImpl.java
@@ -1,6 +1,7 @@
 package com.customer.service;
 
 import com.customer.domain.User;
+import com.customer.exception.UserNotFoundException;
 import com.customer.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,15 +15,23 @@ public class LoginServiceImpl implements LoginService {
 
     @Override
     public User userLogin(User user) {
-        User response = userRepository.findByUsername(user.getUsername()).get();
-        // .orElseThrow(NotFoundException("User Not Found with username: " +
-        // user.getUsername()));
+        User response = null;
 
-        if (user.getPassword().equals(response.getPassword()))
-            return response;
-        // else {
-        // throw new Exception("Username or Password is incorrect");
-        // }
+        try {
+            response = userRepository.findByUsername(user.getUsername())
+                    .orElseThrow(
+                            () -> new UserNotFoundException("User Not Found with username: " + user.getUsername()));
+        } catch (UserNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        if (!(user.getPassword().equals(response.getPassword()))) {
+            try {
+                throw new Exception("Username or Password is incorrect");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 
         return response;
     }
